fee91ec9d360687e18f9fb732e4f508d151625b0
FlavyDev <flavysouza120@gmail.com>
2024-09-07 12:39:40 -0300

    vai

--- projeto_final/bin/activate.fish
+++ projeto_final/bin/activate.fish
@@ -29,7 +29,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/home/flaviane120/[eng_software]/projeto_final"
+set -gx VIRTUAL_ENV "/home/flaviane120/automacao_mercado/projeto_final"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
